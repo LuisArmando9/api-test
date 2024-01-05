8e5e3aa9fed69587533c12ea3dcac23588b8d7d3
luis armando hernandez <luis.hernandez@finvero.com>
2024-01-05 11:08:01 -0600

    add new file to fix

--- src/shopping/aplication/query/implementation/get-shopping-query.ts
+++ src/shopping/aplication/query/implementation/get-shopping-query.ts
@@ -1,7 +1,5 @@
 import { IQuery } from '@nestjs/cqrs';
-import { SearchBrandDto } from 'src/brand/infrestructure/dtos/search-brand-dto';
-import { SearchProductDto } from 'src/products/infrestructure/dtos/search-product-dto';
-import { SearchShoppingDto } from 'src/Shopping/infrestructure/dtos/search-Shopping.dto';
+import { SearchShoppingDto } from 'src/shopping/infrestructure/dtos/search-Shopping.dto';
 
 export class GetShoppingQuery implements IQuery {
   constructor(readonly dto: SearchShoppingDto, readonly user_id: number) {}
